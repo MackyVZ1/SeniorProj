06fc0aa5b136fc257a99146cdc579e6a0caa472d
MackyVZ1 <152200588+MackyVZ1@users.noreply.github.com>
2024-10-30 16:45:43 +0700

    Add tailwind into the project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,7 +6,9 @@ function App() {
 
   return (
     <>
-      
+      <h1 className = "text-3xl text-red-500 font-bold underline">
+      Hello world!
+      </h1>
     </>
   )
 }

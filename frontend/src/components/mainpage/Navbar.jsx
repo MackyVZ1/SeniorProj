e20565ecd8febc15d722f7b18639052d52943bcc
MackyVZ1 <152200588+MackyVZ1@users.noreply.github.com>
2024-12-02 13:58:46 +0700

    edit frontend

--- frontend/src/components/mainpage/Navbar.jsx
+++ frontend/src/components/mainpage/Navbar.jsx
@@ -1,7 +1,6 @@
-import {React, useState} from "react";
+import { React, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Login from "./Login";
-
 
 export default function Navbar() {
   // ลิงค์รูป
@@ -11,24 +10,30 @@ export default function Navbar() {
     "https://nclist.fiet.kmutt.ac.th/images/nclist2022/logo/04_logo_NU.png";
 
   // ตัวแปรใช้ย้ายหน้าเว็บ
-  const nav = useNavigate()
+  const nav = useNavigate();
 
   // ตัวแปรเก็บค่าต่างๆ
-  const [logging, setLogging] = useState(false) // เก็บข้อมูลจะเปิดหน้าล็อคอิน
+  const [logging, setLogging] = useState(false); // เก็บข้อมูลจะเปิดหน้าล็อคอิน
 
   // เมิ่อกดปุ่ม ลงชื่อเข้าใช้
   const openPopup = () => {
-    setLogging(true)
-  }
+    setLogging(true);
+  };
 
   // กดปิดหน้าต่างลงชื่อเข้าใช้
   const closePopup = () => {
-    setLogging(false)
-  }
+    setLogging(false);
+  };
 
   return (
-    <nav className="bg-red-500 p-24">
-
+    <nav className="bg-red-500 shadow-red-600 shadow-lg flex justify-between sm:flex sm:justify-between sticky top-0 z-50 md:flex md:justify-between">
+      <div className="bg-white flex justify-between p-6 -skew-x-[-24deg]"
+      style={{ marginLeft: "-40px"}}>
+        <img className="w-24 h-24 skew-x-[-24deg] ml-8 my-1" src={enLogo} />
+        <img className="w-24 h-24 skew-x-[-24deg] ml-8 my-1" src={eduLogo} />
+      </div>
+      <div className="bg-white p-6 border-8 border-black"></div>
+      <div className="bg-white p-6 border-8 border-black"></div>
     </nav>
   );
 }

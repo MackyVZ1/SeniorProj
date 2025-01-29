62857351a0680543cd25a7695915261e535736a3
MackyVZ1 <152200588+MackyVZ1@users.noreply.github.com>
2025-01-29 13:57:59 +0700

    change the project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,16 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
-import Mainpage from './components/Mainpage'
-import Aboutus from './components/Aboutus'
-import Floristry from './components/game/Floristry';
+
 function App() {
-  
+  const [count, setCount] = useState(0)
+
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Mainpage/>}/>
-          <Route path="/aboutus" element={<Aboutus/>}/>
-          <Route path="/floristry" element={<Floristry/>}/>
-        </Routes>
-      </Router>
+      <p className="text-amber-300">Hello World!</p>
     </>
   )
 }
 
-export default App;
+export default App

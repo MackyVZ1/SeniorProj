59143c862bff2a3c5d40631c198a5882b2928800
MackyVZ1 <152200588+MackyVZ1@users.noreply.github.com>
2025-02-03 16:23:57 +0700

    edit backend | using database online for testing

--- backend/database.js
+++ backend/database.js
@@ -1,13 +1,27 @@
 const mysql      = require('mysql');
 const connection = mysql.createPool({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'dentistrydatabase',
+  host     : 'becct8wm9kkmgla1znpc-mysql.services.clever-cloud.com',
+  user     : 'uni7obbbmtobdmpz', 
+  password : 's9zqbpH6eCR3BU4Ww1gD', 
+  database : 'becct8wm9kkmgla1znpc', 
   port : 3306,
   connectionLimit:100
 });
+// สำหรับทดสอบออนไลน์
+//   host     : 'becct8wm9kkmgla1znpc-mysql.services.clever-cloud.com',
+//   user     : 'uni7obbbmtobdmpz', 
+//   password : 's9zqbpH6eCR3BU4Ww1gD', 
+//   database : 'becct8wm9kkmgla1znpc', 
+//   port : 3306,
+//   connectionLimit:100
 
+// สำหรับทดสอบแบบ localhost
+//   host     : 'localhost', 
+//   user     : 'root', 
+//   password : '',
+//   database : 'dentistrydatabase',
+//   port : 3306,
+//   connectionLimit:100
 try{
     connection.getConnection((err) => {
         // ถ้าเชื่อมต่อไม่ได้
